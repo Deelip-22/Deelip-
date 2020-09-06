3db40120e18970a93f311797d4d832895bd973b8
Deelip-22 <70627583+Deelip-22@users.noreply.github.com>
2020-09-06 16:46:16 +0545

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+    int guess_num;
+    std::cout<<"Please guess a number from 1 to 10:"<<std::endl;
+    std::cin>>guess_num;
+    std::cout<<"The number is:"<<guess_num<<std::endl;
+    return 0;
+}
+
+
